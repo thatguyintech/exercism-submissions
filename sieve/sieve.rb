5fc1b9c978a2e847b572a9858a089accf927a6e9
Albert Hu <alberthu@berkeley.edu>
2016-07-07 10:14:07 -0700

    Update solution for #8 Sieve

--- sieve/sieve.rb
+++ sieve/sieve.rb
@@ -1,14 +1,21 @@
 class Sieve
     def initialize(limit)
-        @range = (2..limit).to_a
-        @primes = []
+        @primes = sieve_up_to(limit)
     end
 
     def primes
-        while current_sieve = @range.shift
-            @primes << current_sieve
-            @range.reject! {|number| number % current_sieve == 0}
-        end
         @primes
+    end
+    
+    def sieve_up_to(limit)
+        primes = []
+        range = (2..limit).to_a
+
+        while current_sieve = range.shift
+            primes << current_sieve
+            range.reject! {|number| number % current_sieve == 0}
+        end
+        
+        @primes = primes
     end
 end
